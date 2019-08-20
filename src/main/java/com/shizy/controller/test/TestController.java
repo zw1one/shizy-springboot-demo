22be39a37a2a2b9a885bd7f75779a3bf4878b047
shizhongyu <shizhongyu@yibaogao.com>
2019-08-20 17:06:08 +0800

    add todo and test

---
+++ src/main/java/com/shizy/controller/test/TestController.java
@@ -0,0 +1,70 @@
+package com.shizy.controller.test;
+
+import com.shizy.service.user.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@Api(tags = "user-demo", description = "user CRUD")
+public class TestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @Autowired
+    private UserService userService;
+
+    @ApiOperation(value = "user add", notes = "")
+    @RequestMapping(value = "/user/add", method = RequestMethod.POST)
+    public String add(@RequestBody Map param) {
+
+
+        /**
+         * todo
+         *
+         * 1、redis
+         *
+         * 2、mongodb
+         *
+         * 3、log
+         *
+         *
+         *
+         */
+
+
+        return "";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,7 +1,7 @@
 
 
 
-public class test {
+public class MyTest {
 
 
     public static void main(String[] args) {
